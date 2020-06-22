de87ec22b3bdf9f3f2a4d596aec34773cfb065f8
Admin <241538nxdd@gmail.com>
2020-06-22 19:58:40 +0700

    Angular property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  template: `
+    <h1 [class.with-border]="withBorder">{{title}}</h1>
+    <img [src]="imageSrc">
+    `,
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
-  title = 'ng-coders-tokyo';
+  title = 'Coders.Tokyo Updated';
+  imageSrc = 'https://picsum.photos/id/237/200';
+
+  textColor = 'tomato';
+  backgroundColor = 'black';
+  styleObj = {color: this.textColor, background: this.backgroundColor};
+
+  withBorder = false;
 }
